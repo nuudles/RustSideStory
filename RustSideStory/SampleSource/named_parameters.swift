52f150795379b49fb65b1fd37fe0361862fb441f
Christopher Luu <nuudles@gmail.com>
2016-10-26 22:23:22 -0400

    Remove irrelevant comment

--- RustSideStory/SampleSource/named_parameters.swift
+++ RustSideStory/SampleSource/named_parameters.swift
@@ -3,7 +3,6 @@
 let string = "Hello world"
 print("\(string.replacingOccurrences(of: "e", with: "o"))")
 
-// Extension is like an `impl` without a `trait`
 extension String {
 	func replace(_ needle: String, _ replacement: String) -> String {
 		return replacingOccurrences(of: needle, with: replacement)
